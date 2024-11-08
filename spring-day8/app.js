17edf6d7ef8c6165ad6748cf4e7f26f39e087de5
Vishwas K Singh <vishwasks32@gmail.com>
2024-11-08 09:44:22 +0530

    spring day 8

---
+++ spring-day8/app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+
+const appName = "CloudThat Labs"
+const programName = "Deloitte Platform Integration"
+const technologyName = "NodeJS"
+
+// app.set('views', './views'); // Assuming views directory is in the same directory as your app
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + "/public"));
+
+//Rendering index.ejs 
+app.get('/', (req, res) => {
+    res.render('index', { appName, programName, technologyName });
+});
+
+// app.get('/home', (req, res) => {
+//     res.render('home', { name: 'CloudThat Labs' });
+// });
+
+app.get('*', (req, res) => {
+    const path = req.path.replace(/^\//, '');
+    console.log(path);
+
+    res.render(`${path}.ejs`, { name: 'CloudThat Labs' }); // Assuming your templates are in the same directory as the app
+});
+
+
+app.listen(5000, () => {
+    console.log(`App listening at port 5000`);
+})
